58dbc2ab1b1c0cf9e68bd02e163ccf763229bc4f
Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
2019-12-29 22:35:10 -0800

    Update 1-intro.js

--- 1-intro.js
+++ 1-intro.js
@@ -1,3 +1,4 @@
 // show the name, continent and population of all countries.
+// world is our table containing all countries: their names, continent, population and area 
 
 SELECT name, continent, population FROM world;
